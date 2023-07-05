11477de96c1944923e7ac9e23b4306ec76b2d4ec
zoemurie <zoe.murie@gmail.com>
2023-07-05 14:38:46 +0200

    Initial commit of project.

---
+++ exemplemaven2/src/main/java/com/openclassrooms/installeride/ExempleMaven2.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.installeride;
+
+import ij.IJ;
+import ij.ImagePlus;
+public class ExempleMaven2 {
+
+	public static void main(String[] args) {
+        ImagePlus imp = IJ.openImage(); // Choisit un fichier et stocke le résultat dans imp
+        imp.show(); // Affiche l'image à l'écran
+		
+
+	}
+
+}
